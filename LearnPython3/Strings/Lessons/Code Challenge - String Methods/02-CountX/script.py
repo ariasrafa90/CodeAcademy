1292c7172f4b20a57a1d9370ce23936339f13e69
Rafael Arias <ariasrafa90@gmail.com>
2021-01-29 21:20:15 -0300

    Deleting DS_Store files

---
+++ LearnPython3/Strings/Lessons/Code Challenge - String Methods/02-CountX/script.py
@@ -0,0 +1,15 @@
+#Write a function named count_char_x that takes a string named word and a single character named x as parameters. The function should return the number of times x appears in word.
+
+# Write your count_char_x function here:
+def count_char_x(word, x):
+  occurrences = 0
+  for letter in word:
+    if letter == x:
+      occurrences += 1
+  return occurrences
+
+# Uncomment these function calls to test your tip function:
+print(count_char_x("mississippi", "s"))
+# should print 4
+print(count_char_x("mississippi", "m"))
+# should print 1
